cf8fe3b8ab66b64b2acf38ee8bbd3e4560dcf9a0
BBO <bouchtabadreddine@gmail.com>
2020-09-29 22:01:45 +0200

    Todo app first commit

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [todos , setTodos] = useState([
+    {text:'buy coffee' , id: '1'},
+    {text:'create an app' , id: '2'},
+    {text:'buy a kebab' , id: '3'},
+    {text:'sleep well' , id: '4'},
+    {text:'do some sport' , id: '5'},
+    {text:'stop being tired' , id: '6'},
+  ]);
+
+  const pressHandler = (id) => {
+    setTodos((prevPeople)=> {
+      return prevPeople.filter((person)=>(person.id != id))
+    })
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        keyExtractor={ (item) => item.id}
+        data={todos}
+        renderItem={({item}) => (
+          <TouchableOpacity onPress={()=> pressHandler(item.id)}>
+            <Text style={styles.text}>{item.text}</Text>
+          </TouchableOpacity>
+        )}
+      />
+
+      {/* <ScrollView>
+        {people.map(item => (
+          <View key={item.key} style={styles.list}>
+            <Text style={styles.text}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
@@ -15,7 +44,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:40,
+    paddingHorizontal:4,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  list: {
+    backgroundColor:'pink',
+    padding:20,
+    marginTop:20,
+  },
+  text:{
+    fontSize:24,
+    backgroundColor:'pink',
+    padding:20,
+    margin:20,
   },
 });
